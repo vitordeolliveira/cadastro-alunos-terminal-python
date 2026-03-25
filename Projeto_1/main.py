656d65aeb0e1800f68995504d4f0d093a69fca56
Vitor De Oliveira <vitordeoliveira.200624@gmail.com>
2026-03-25 17:17:23 -0300

    Modulurazidando

--- Projeto_1/main.py
+++ Projeto_1/main.py
@@ -1,4 +1,48 @@
 from visual import menu
+import time
+
+def adicionar_aluno():
+    nome = input("DIGITE O NOME DO ALUNO: ").strip().upper()
+    idade = int(input(f"QUAL É A IDADE DO/A {nome}: "))
+    nota = float(input(f"QUAL FOI A NOTA DO/A {nome}: "))
+    dicionario_escolar = {
+    "Nome Do Aluno":nome,
+    "Idade Do Aluno":idade,
+    "Nota Do Aluno":nota
+}
+    lista_alunos.append(dicionario_escolar)
+
+def media_turma():
+    menu.limpartela()
+    md = 0
+    if len(lista_alunos) == 0:
+        return "NAO HÁ ALUNOS REGISTRADOS"
+    soma = 0
+    for aluno in lista_alunos:
+        soma += aluno['Nota Do Aluno']
+    md = soma / len(lista_alunos)
+    return f"A MEDIA DA TURMA É {md:.2f}"
+
+def remover_aluno():
+    menu.limpartela()
+    remover = input("QUAL ALUNO DESEJA REMOVER: ").strip().upper()
+    for aluno in lista_alunos:
+        if aluno["Nome Do Aluno"] == remover:
+            lista_alunos.remove(aluno)
+            return f"Aluno:{remover} REMOVIDO COM SUCESSO"
+    else:
+        return f"Aluno:{remover} NAO EXISTE"
+
+def buscar():
+    menu.limpartela()
+    busca = input("QUAL ALUNO DESEJA PROCURAR: ").strip().upper()
+    for aluno in lista_alunos:
+        if aluno["Nome Do Aluno"] == busca:
+            return f"Aluno:{busca} ENCONTRADO/A"
+    else:
+        return f"Aluno:{busca} NAO FOI ENCONTRADO/A"
+        
+
 
 lista_alunos = []
 while True:
@@ -6,17 +50,21 @@ while True:
     valor = menu.tabela()
     match valor:
         case 1:
-           menu.adicionar_aluno()
+           adicionar_aluno()
         case 2:
             menu.limpartela()
             for alunos in lista_alunos:
                 print(alunos["Nome Do Aluno"])
+            time.sleep(2)
 
         case 3:
-            print (menu.buscar())      
+            print (buscar()) 
+            time.sleep(2)     
         case 4:
-            print (menu.remover_aluno())
+            print (remover_aluno())
+            time.sleep(2)
         case 5:
-            print (menu.media_turma())
+            print (media_turma())
+            time.sleep(2)
         case _:
             break
--- Projeto_1/visual/menu.py
+++ Projeto_1/visual/menu.py
@@ -1,5 +1,4 @@
 import os
-from main import lista_alunos
 def limpartela():
     os.system('cls' if os.name == 'nt' else 'clear')
 
@@ -11,49 +10,10 @@ def tabela():
     try:
         x = int(input('Escolha: '))
     except Exception as erro:
-        print(f'ERRO!!! {erro}')
+        print(f'ERRO!!! {erro.__class__}')
         print('Por Favor Digite Um Numero Inteiro Valído: ')
         x = int(input('Tente Novamente: '))
     return x
 
-def adicionar_aluno():
-    nome = input("DIGITE O NOME DO ALUNO: ").strip().upper()
-    idade = int(input(f"QUAL É A IDADE DO/A {nome}: "))
-    nota = float(input(f"QUAL FOI A NOTA DO/A {nome}: "))
-    dicionario_escolar = {
-    "Nome Do Aluno":nome,
-    "Idade Do Aluno":idade,
-    "Nota Do Aluno":nota
-}
-    lista_alunos.append(dicionario_escolar)
 
-def media_turma():
-    limpartela()
-    md = 0
-    if len(lista_alunos) == 0:
-        return "NAO HÁ ALUNOS REGISTRADOS"
-    soma = 0
-    for aluno in lista_alunos:
-        soma += aluno['Nota Do Aluno']
-    md = soma / len(lista_alunos)
-    return f"A MEDIA DA TURMA É {md:.2f}"
-
-def buscar():
-    limpartela()
-    busca = input("QUAL ALUNO DESEJA PROCURAR: ").strip().upper()
-    for aluno in lista_alunos:
-        if aluno["Nome Do Aluno"] == busca:
-            return f"Aluno:{busca} ENCONTRADO/A"
-    else:
-        return f"Aluno:{busca} NAO FOI ENCONTRADO/A"
-        
-def remover_aluno():
-    limpartela()
-    remover = input("QUAL ALUNO DESEJA REMOVER: ").strip().upper()
-    for aluno in lista_alunos:
-        if aluno["Nome Do Aluno"] == remover:
-            lista_alunos.remove(aluno)
-            return f"Aluno:{remover} REMOVIDO COM SUCESSO"
-    else:
-        return f"Aluno:{remover} NAO EXISTE"
                 
